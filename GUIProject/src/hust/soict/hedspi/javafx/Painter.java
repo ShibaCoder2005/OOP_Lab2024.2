e4fbc650a2c5a92dad8082b0a0550a9130782f3a
ShibaCoder2005 <110546298+ShibaCoder2005@users.noreply.github.com>
2025-05-30 14:43:23 +0700

    Update Painter.java

--- GUIProject/src/hust/soict/hedspi/javafx/Painter.java
+++ GUIProject/src/hust/soict/hedspi/javafx/Painter.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class Painter extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/hust/soict/program/javafx/Painter.fxml")));
+		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/hust/soict/hedspi/javafx/Painter.fxml")));
 		Scene scene = new Scene(root);
 		stage.setTitle("Painter");
 		stage.setScene(scene);
